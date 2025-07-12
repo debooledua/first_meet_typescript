c52fb4840d029fc97a3c907af68185f38ecfbb3d
d_z <98271110+debooledua@users.noreply.github.com>
2025-07-12 18:38:49 +0300

    Delete tempCodeRunnerFile.ts

--- tempCodeRunnerFile.ts
+++
@@ -1,4 +0,0 @@
-let a = Number(prompt('Введіть 1 - сторону: '));
-let b = Number(prompt('Введіть 2 - сторону: '));
-let c = Number(prompt('Введіть 3 - сторону: '));
- if (isNaN(a) || isNaN(b) || isNaN(c) )
